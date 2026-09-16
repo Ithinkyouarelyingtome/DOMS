8f5e2b2fe9ec1cd0213c5faa5d1c662c714c892a
Ithinkyouarelyingtome <sonu676q@gmail.com>
2026-09-17 00:16:22 +0530

    ui_update

--- webui/app.py
+++ webui/app.py
@@ -1,9 +1,32 @@
-from flask import Flask, jsonify
-from Backend.storage_manager import Storagemanager
-from Backend.config import STORAGE_DIR, STORAGE_QUOTA
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return 'DOMS Web UI is running'
+    return render_template("index.html")
+
+
+@app.route("/storage")
+def storage():
+    return render_template("storage.html")
+
+@app.route("/devices")
+def devices():
+    return render_template("device.html")
+
+@app.route("/transfers")
+def transfers():
+    return render_template("transfers.html")
+
+@app.route("/network")
+def network():
+    return render_template("network.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)

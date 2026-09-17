c4e74cb2f97074193c767e92e3e9b4d645885c4f
Ithinkyouarelyingtome <sonu676q@gmail.com>
2026-09-17 12:21:17 +0530

    ui_upgrade

--- webui/app.py
+++ webui/app.py
@@ -5,28 +5,33 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", active_page="home")
 
 
 @app.route("/storage")
 def storage():
-    return render_template("storage.html")
+    return render_template("storage.html", active_page="storage")
+
 
 @app.route("/devices")
 def devices():
-    return render_template("device.html")
+    return render_template("device.html", active_page="devices")
+
 
 @app.route("/transfers")
 def transfers():
-    return render_template("transfers.html")
+    return render_template("transfers.html", active_page="transfers")
+
 
 @app.route("/network")
 def network():
-    return render_template("network.html")
+    return render_template("network.html", active_page="network")
+
 
 @app.route("/settings")
 def settings():
-    return render_template("settings.html")
+    return render_template("settings.html", active_page="settings")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
